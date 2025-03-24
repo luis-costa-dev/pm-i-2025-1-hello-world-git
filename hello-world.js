8d97e7e3f6dc52acf2ab63e4cd93f3f9fc6f70e1
luis <ecluisfilipe@gmail.com>
2025-03-24 20:45:53 -0300

    pronto

--- hello-world.js
+++ hello-world.js
@@ -6,6 +6,24 @@ nameInput.onkeyup = (event) => {
         messageP.innerText = '';
     }
 }
+nameInput.addEventListener('keydown', function(event){
+    if (event.key === 'Enter') {
+        messageP.innerText = `Hello World, ${nameInput.value}!`;
+    }
+}
+)
+
+nameInput.addEventListener('keydown', function(event){
+    if (event.key === 'Escape') {
+        nameInput.value = '';
+    }
+}
+)
+
+const Apagar = () =>{
+    nameInput.value = '';
+    messageP.innerText = '';
+}
 
 const helloWorld = () => {
     messageP.innerText = `Hello World, ${nameInput.value}!`;
